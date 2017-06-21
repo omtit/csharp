1bcf825b594bb84697f3e0e865299f9a5bb5807b
nickoryakov <niros_2024@mail.ru>
2017-06-21 20:37:02 +0600

    лр5 задача 11

---
+++ lab5/task11/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string t;
+           
+            int a, b, c; 
+            t = Console.ReadLine();
+            a = int.Parse(t);
+            t = Console.ReadLine();
+            b = int.Parse(t);
+            t = Console.ReadLine();
+            c = int.Parse(t);
+
+            if (a<b&&b<c)
+            {
+                Console.WriteLine("Выполняется");
+            }
+            else
+            {
+                Console.WriteLine("Не выполняется");
+            }
+        }
+    }
+}
