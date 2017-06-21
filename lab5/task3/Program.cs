77892aa9abbdd07e9476b5d9de3612bcba1d4969
nickoryakov <niros_2024@mail.ru>
2017-06-21 20:19:12 +0600

    лр 5 задание 3

---
+++ lab5/task3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string t;
+            t = Console.ReadLine();
+            int k;
+            k = int.Parse(t);
+            double i;
+            if (k < 0)
+            {
+                i = k * k;
+                Console.WriteLine(i);
+            }
+            else
+            {
+                i = Math.Sqrt(k);
+                Console.WriteLine(i);
+            }
+            
+            
+                
+            
+        }
+    }
+}
