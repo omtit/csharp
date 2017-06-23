f04129b25600ec484d863e02c9823291e650c553
nickoryakov <niros_2024@mail.ru>
2017-06-23 20:14:57 +0600

    Лр 5 задание 4

---
+++ lab5/task4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string t;
+            t = Console.ReadLine();
+            int a;
+            a = int.Parse(t);
+            int b;
+            b = Math.Abs(a);
+            Console.WriteLine(b);
+        }
+    }
+}
