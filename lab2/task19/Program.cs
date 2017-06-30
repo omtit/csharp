8b2e588a6c09939fbd126ad20ec54d8997453d84
nickoryakov <niros_2024@mail.ru>
2017-06-30 19:46:59 +0600

    лр 2 задание 19

---
+++ lab2/task19/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string t;
+            double vel, time, acc;
+            t = Console.ReadLine();
+            vel = double.Parse(t);
+            t = Console.ReadLine();
+            time = double.Parse(t);
+            t = Console.ReadLine();
+            acc = double.Parse(t);
+
+            double dist;
+            dist = vel * time + ((acc*(time*time))/2);
+            Console.WriteLine("{0:F4}", dist);
+        }
+    }
+}
