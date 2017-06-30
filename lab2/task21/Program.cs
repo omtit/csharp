8f07f069a8c40d3298f7c1617e1e3262e35cc168
nickoryakov <niros_2024@mail.ru>
2017-06-30 20:16:47 +0600

    лр 2 задание 20

---
+++ lab2/task21/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string t;
+            double r1, r2;
+            t = Console.ReadLine();
+            r1 = double.Parse(t);
+            t = Console.ReadLine();
+            r2 = double.Parse(t);
+
+            if (r1 == 0)
+            {
+                Console.WriteLine("r1 должно быть не равно 0");
+                return;
+            }
+            if (r2 == 0)
+            {
+                Console.WriteLine("r2 должно быть не равно 0");
+                return;
+            }
+            double res;
+            res = (1 / r1) + (1 / r2);
+
+            Console.WriteLine("{0:F4}", res);
+        }
+    }
+}
