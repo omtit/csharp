3313fd26913762d009a55b0a41af95453d675762
nickoryakov <niros_2024@mail.ru>
2017-07-04 19:16:53 +0600

    лр 5 задание 8

---
+++ lab5/task8/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string t;
+            int a, b, c;
+            t = Console.ReadLine();
+            a = int.Parse(t);
+            t = Console.ReadLine();
+            b = int.Parse(t);
+            t = Console.ReadLine();
+            c = int.Parse(t);
+
+            if (a == 0)
+            {
+                Console.WriteLine("Данное уравнение не является квадратным");
+                return;
+            }
+            int dis;
+            dis = b * b - 4 * (a * c);
+            
+            if (dis>0)
+            {
+                Console.WriteLine("У уравнения {0}x^2+{1}x+{2}=0 два вещественных корня", a, b, c);
+                return;
+            }
+            if (dis == 0)
+            {
+                Console.WriteLine("У уравнения {0}x^2+{1}x+{2}=0 один вещественный корень", a, b, c);
+                return;
+            }
+            if (dis < 0)
+            {
+                Console.WriteLine("У уравнения {0}x^2+{1}x+{2}=0 нет вещественных корней", a, b,c);
+                return;
+            }
+        }
+    }
+}
