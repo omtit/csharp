edecce0bf2317c3aa7be67606cde9f70029b573c
nickoryakov <niros_2024@mail.ru>
2017-07-06 19:47:41 +0600

    лр 5 задание 17

---
+++ lab5/task17/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string t;
+            int M, P;
+            t = Console.ReadLine();
+            M = int.Parse(t);
+            t = Console.ReadLine();
+            P = int.Parse(t);
+
+            if (P == 0)
+            {
+                Console.WriteLine("Значение P не должно быть равно нулю");
+                return;
+            }
+
+            int MP;
+            MP = M / P;
+
+            
+            
+            if (M % P > 0)
+            {
+                Console.WriteLine("Не делится без остатка");
+                return;
+            }
+            if (M % P == 0)
+            {
+                Console.WriteLine(MP);
+                return;
+            }
+        }
+    }
+}
