bf7352d4741b2b4bdfb508d6a8e9a7456dc0992b
nickoryakov <niros_2024@mail.ru>
2017-07-06 18:21:38 +0600

    лр 5 задание 10

---
+++ lab5/task10/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string t;
+            double x, y, z;
+            t = Console.ReadLine();
+            x = double.Parse(t);
+            t = Console.ReadLine();
+            y = double.Parse(t);
+            t = Console.ReadLine();
+            z = double.Parse(t);
+
+            if (x > 0)
+            {
+                Console.WriteLine(x * x);
+            }
+            else
+            {
+                Console.WriteLine(x);
+            }
+
+
+            if (y > 0)
+            {
+                Console.WriteLine(y * y);
+            }
+            else
+            {
+                Console.WriteLine(y);
+            }
+
+            if (z > 0)
+            {
+                Console.WriteLine(z * z);
+            }
+            else
+            {
+                Console.WriteLine(z);
+            }
+        }
+    }
+}
