de7a90e40771d4ac174b09f50ca7733345f4c685
nickoryakov <niros_2024@mail.ru>
2017-07-06 18:48:13 +0600

    лр5 задача 14

---
+++ lab5/task14/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string t;
+            double x, y, z;
+            t = Console.ReadLine();
+            x = double.Parse(t);
+            t = Console.ReadLine();
+            y = double.Parse(t);
+            t = Console.ReadLine();
+            z = double.Parse(t);
+
+            if (-3 < x && x < 5)
+            {
+                Console.Write(x+" ");
+            }
+            else
+            {
+                Console.Write(" ");
+            }
+
+            if (-3 < y && y < 5)
+            {
+                Console.Write(y+" ");
+            }
+            else
+            {
+                Console.Write(" ");
+            }
+
+            if (-3 < z && z < 5)
+            {
+                Console.Write(z + " ");
+            }
+            else
+            {
+                Console.Write(" ");
+            }
+        }
+    }
+}
