924d76413481a3e72a0a3acc8a6868a7614247ac
nickoryakov <niros_2024@mail.ru>
2017-07-06 18:31:37 +0600

    лр 5 задание 13

---
+++ lab5/task13/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string t;
+            int x, y, z;
+            t = Console.ReadLine();
+            x = int.Parse(t);
+            t = Console.ReadLine();
+            y = int.Parse(t);
+            t = Console.ReadLine();
+            z = int.Parse(t);
+
+            if (x != y && y != z && z != x)
+            {
+                Console.WriteLine("Числа не равны друг другу");
+                return;
+            }
+
+            if (x == y || y == z || z == x)
+            {
+                Console.WriteLine("Среди чисел есть равные");
+                return;
+            }
+        }
+    }
+}
