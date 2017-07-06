8babfbae20b1feeee682dec202ab40ba412687b7
nickoryakov <niros_2024@mail.ru>
2017-07-06 19:19:26 +0600

    лр 5 задание 16

---
+++ lab5/task16/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string t;
+            int x, y, z;
+            t = Console.ReadLine();
+            x = int.Parse(t);
+            t = Console.ReadLine();
+            y = int.Parse(t);
+            t = Console.ReadLine();
+            z = int.Parse(t);
+
+            if (x > y && x > z)
+            {
+                Console.WriteLine(x);
+                return;
+            }
+            if (y > x && y > z)
+            {
+                Console.WriteLine(y);
+                return;
+            }
+            if (z > x && z > y)
+            {
+                Console.WriteLine(z);
+                return;
+            }
+        }
+    }
+}
