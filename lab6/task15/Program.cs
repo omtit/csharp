fc0d37ff07309d8b6135c00a2d1731e7bcedf333
nickoryakov <niros_2024@mail.ru>
2017-07-13 19:30:13 +0600

    лр 6 задание 15

---
+++ lab6/task15/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            x = 20;
+            y = 40;
+
+            int i = x;
+            int a;
+            int sum = 0;
+            while (i <= y)
+            {
+                a = i * i * i;
+                i = i + 1; 
+                sum = sum + a;
+                                              
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
