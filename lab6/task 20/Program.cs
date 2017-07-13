08d6abb87ae8d78bdb20b2a66bf36588a5f2c48b
nickoryakov <niros_2024@mail.ru>
2017-07-13 20:07:40 +0600

    лр 6 задание 20

---
+++ lab6/task 20/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string t;
+            int a, b;
+            t = Console.ReadLine();
+            a = int.Parse(t);
+            t = Console.ReadLine();
+            b = int.Parse(t);
+
+            int i, n;
+           
+            long sum = 0;
+
+            if (b > a)
+            {
+                i = a;
+                while (i <= b)
+                {
+                    n = i * i;
+                    i = i + 1;
+                    sum = sum + n;
+                }
+            }
+            else
+            {
+                i = b;
+                while (i <= a)
+                {
+                    n = i * i;
+                    i = i + 1;
+                    sum = sum + n;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
