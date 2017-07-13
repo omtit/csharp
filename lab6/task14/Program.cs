866e44e03f1f0536a880e066cfecf01c67e9a966
nickoryakov <niros_2024@mail.ru>
2017-07-13 18:27:47 +0600

    лр 6 задача 14

---
+++ lab6/task14/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string t;
+            t = Console.ReadLine();
+            int a, b;
+            a = int.Parse(t);
+            t = Console.ReadLine();
+            b = int.Parse(t);
+
+            long product = 1;
+            int n;
+
+            if (a > b)
+            {
+                n = b;
+                while (n <= a)
+                {
+                    
+                    product = product * n;
+                    n = n + 1;
+                }
+                
+            }
+            n = a;
+
+            while (n <= b)
+            {
+
+                product = product * n;
+                n = n + 1;
+            }
+            Console.WriteLine(product);
+        }
+    }
+}
