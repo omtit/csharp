fc8eaf6ad0adfd2f83385c5355726d7fb8afae7f
nickoryakov <niros_2024@mail.ru>
2017-07-20 19:29:10 +0600

    лр 6 задание 23 и 24

---
+++ lab6/task24/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string t;
+            int x;
+            t = Console.ReadLine();
+            x = int.Parse(t);
+
+            int a = 30;
+            int b = 40;
+
+            while (a <= b)
+            {
+                if (x == a)
+                {
+                    Console.WriteLine("{0}+", a);
+                }
+                else
+                {
+                    Console.WriteLine("{0}-" ,a);
+                }
+                a = a + 1;
+            }
+        }
+    }
+}
