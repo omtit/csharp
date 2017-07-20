08088701dcef0a0cad2affeba719eea48738d64e
nickoryakov <niros_2024@mail.ru>
2017-07-20 19:57:31 +0600

    лр 6 задание 25

---
+++ lab6/task25/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string t;
+            int y;
+            t = Console.ReadLine();
+            y = int.Parse(t);
+
+            if (y < 0)
+            {
+                Console.WriteLine("Значение y должно быть в интервале [0 ; 15]");
+                return;
+            }
+            if (y > 15)
+            {
+                Console.WriteLine("Значение y должно быть в интервале [0 ; 15]");
+                    return;
+            }
+
+            int i = 0;
+
+            while (i < 15)
+            {
+                i = i + 1;
+
+                if (i <= y)
+                {
+                    Console.Write("#");
+                }
+                else
+                {
+                    Console.Write(".");
+                }
+            }
+        }
+    }
+}
