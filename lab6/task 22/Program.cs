0142cad5c6846f11c22e5bd2bd402af1521706ac
nickoryakov <niros_2024@mail.ru>
2017-07-20 18:49:37 +0600

    лр 6 задание 22

---
+++ lab6/task 22/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string t;
+            int n;
+            t = Console.ReadLine();
+            n = int.Parse(t);
+
+            int i = 0;
+            int i1 = 0;
+
+            if (n < 0)
+            {
+                Console.WriteLine("Значение n должно быть не отрицательным");
+                return;
+            }
+
+            while (i < n)
+            {
+                Console.Write("!");
+                i = i + 1;
+                i1 = i1 + 1;
+
+                if (i1 == 5)
+                {
+                    Console.Write(" ");
+                    i1 = i1 - 5;
+                }
+            }
+        }
+    }
+}
