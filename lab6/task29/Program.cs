96956a1e8f2feb4b14b6afad1eb0e790bea6f9d6
nickoryakov <niros_2024@mail.ru>
2017-07-27 19:25:26 +0600

    лр 6 update + задание 29

---
+++ lab6/task29/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string t;
+            int y;
+            t = Console.ReadLine();
+            y = int.Parse(t);
+
+            if (y<=0) 
+            {
+                Console.WriteLine("Значение y должно быть положительным");
+                return;
+            }
+            int i = 0;
+            int n = 0;
+
+            while (i<=y)
+            {
+
+                i = i + 1;
+
+            if (y%i==0)
+            {
+                n = n + 1;
+            }
+            }
+
+            if (n == 2)
+            {
+                Console.WriteLine("число простое");
+            }
+            else
+            {
+                Console.WriteLine("число не простое");
+                    return;
+            }
+        }
+    }
+}
