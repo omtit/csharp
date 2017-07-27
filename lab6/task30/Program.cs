6baa28ba744c6ae9c8e55204bbaa4f52b814e2ab
nickoryakov <niros_2024@mail.ru>
2017-07-27 20:02:12 +0600

    лр 6 update + задание 30

---
+++ lab6/task30/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string t;
+            int w;
+            t = Console.ReadLine();
+            w = int.Parse(t);
+
+            if (w < 0)
+            {
+                Console.WriteLine("Значение w должно быть в интервале [0;20]");
+                return;
+            }
+            if (w > 20)
+            {
+                Console.WriteLine("Значение w должно быть в интервале [0;20]");
+                return;
+            }
+
+            int i = 0;
+            
+
+            while (i<w) 
+            {
+                Console.Write("a");                
+                i = i + 1;
+            }
+            Console.WriteLine();
+            i = 0;
+            while (i < w)
+            {
+                Console.Write("b");
+                i = i + 1;
+            }
+            Console.WriteLine();
+            i = 0;
+            while (i < w)
+            {
+                Console.Write("c");
+                i = i + 1;
+            }
+            Console.WriteLine();
+            i= 0;
+            while (i < w)
+            {
+                Console.Write("d");
+                i = i + 1;
+            }
+            Console.WriteLine();
+            i= 0;
+            while (i < w)
+            {
+                Console.Write("e");
+                i = i + 1;
+            }
+            Console.WriteLine();
+            i = 0;
+            while (i < w)
+            {
+                Console.Write("f");
+                i = i + 1;
+            }
+            Console.WriteLine();
+            i = 0;
+            while (i < w)
+            {
+                Console.Write("g");
+                i = i + 1;
+            }
+            Console.WriteLine();
+            i = 0;
+            while (i < w)
+            {
+                Console.Write("h");
+                i = i + 1;
+            }
+            Console.WriteLine();
+        }
+    }
+}
