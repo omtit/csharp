2cb1415ef8f67b74801cdf95be27ea2fcde8db10
nickoryakov <niros_2024@mail.ru>
2017-07-27 19:06:26 +0600

    лр 6 update + задание 28

---
+++ lab6/task28/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string t;
+            int x;
+            t = Console.ReadLine();
+            x = int.Parse(t);
+            int i = 0;
+
+            if (x <= 0)
+            {
+                Console.WriteLine("Значение х должно быть положительным");
+                return;
+            }
+
+            while (i <= x)
+            {
+                i = i+1;
+                if (x%i==0)
+                {
+
+                    Console.Write("{0} ", i);
+                }
+                
+            }
+            
+        }
+    }
+}
