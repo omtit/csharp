fc95ec2634df921ad7c1feec6d39bac215f7c8e1
nickoryakov <niros_2024@mail.ru>
2017-07-27 18:48:08 +0600

    лр 6 update + задание 27

---
+++ lab6/task27/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 100;
+            int b = 1000;
+            int count = 0;
+            
+
+            while (a <= b)
+            {
+               
+                if (a % 13 == 0)
+                {
+                    count = count + 1;
+                    
+                }
+                 a = a + 1;
+               
+            }
+            Console.WriteLine("{0}", count);
+        }
+    }
+}
