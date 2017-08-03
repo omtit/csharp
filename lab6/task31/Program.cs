c1bbf24920e55c25c1f22eb1e00b5ab8e9c54fa6
nickoryakov <niros_2024@mail.ru>
2017-08-03 19:27:46 +0600

    лр 6 update + задание 31

---
+++ lab6/task31/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string t;
+            int w;
+            t = Console.ReadLine();
+            w = int.Parse(t);
+
+            if (w < 0)
+            {
+                Console.WriteLine("Значение w должно быть в интервале [0;20]");
+                return;
+            }
+            if (w > 20)
+            {
+                Console.WriteLine("Значение w должно быть в интервале [0;20]");
+                return;
+            }
+
+            
+            int n = 0;
+            int i = 0;
+
+            while (n <= 9)
+            {
+                i = 0;
+                while (i < w)
+                {
+                    Console.Write(n);
+                    i = i + 1;
+                }
+                n = n + 1;
+               
+                Console.WriteLine();
+            }
+           
+            
+        }
+    }
+}
